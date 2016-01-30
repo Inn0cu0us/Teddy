1ce8d02ec9b21d922bf4f398f138578eb43ddee2
Patrick Boyer <boyer.patrick@gmail.com>
2016-01-30 02:02:19 -0500

    A candle script

--- Assets/Scripts/candel.cs
+++ Assets/Scripts/candel.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class candel : MonoBehaviour {
+public class candel : Usable {
 
 	public Light Flame;
 	public Color CandleColor;
-	
+    public float SecondsOfCandlelight;
+
+    private bool isLit = false;
+    private float timer = 0f;
 	// Use this for initialization
 	void Start () {
 		Flame = GetComponentInChildren<Light> ();
-	}
+        Flame.color = CandleColor;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		Flame.color = CandleColor;
+        if (isLit)
+        {
+            Flame.intensity = 10;
+            timer += Time.deltaTime;
+            if (timer >= SecondsOfCandlelight)
+            {
+                Flame.intensity = 0f;
+                isLit = false;
+            }
+        }
 	}
+
+    public override void PerformUseAction()
+    {
+        isLit = true;
+    }
 }
