2623fb37f8e212713f680f718ae00308f79547cf
Patrick Boyer <boyer.patrick@gmail.com>
2016-01-30 13:45:14 -0500

    lighting, shadows, and candle vfx

---
+++ Assets/Scripts/Candle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candle : Usable {
+
+	public Light Flame;
+	public Color CandleColor;
+    public float SecondsOfCandlelight;
+	public float SecondsBetweenFlicker;
+	public float FlickerRange;
+
+    private bool isLit = false;
+    private float timer = 0f;
+	private float flickerTimer = 0f;
+	private float OriginalIntensity;
+	private MeshRenderer TintRenderer;
+	private Light GlobalLight;
+	private Color GlobalLightStartingColor;
+	void Start () 
+	{
+		Flame = GetComponentInChildren<Light> ();
+        Flame.color = CandleColor;
+		OriginalIntensity = Flame.intensity;
+		var go = GameObject.FindGameObjectWithTag ("GlobalLight");
+		GlobalLight = go.GetComponent<Light> ();
+		GlobalLightStartingColor = GlobalLight.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLit)
+        {
+			flickerTimer += Time.deltaTime;
+			if (flickerTimer >= SecondsBetweenFlicker)
+			{
+				flickerTimer = 0f;
+				Flame.intensity = Random.Range(OriginalIntensity - FlickerRange, OriginalIntensity + FlickerRange);
+
+			}
+            timer += Time.deltaTime;
+            if (timer >= SecondsOfCandlelight)
+            {
+				Extinguish();
+                isLit = false;
+				timer = 0f;
+            }
+        }
+	}
+
+    public override void PerformUseAction()
+    {
+		Debug.Log ("Candle on");
+		Light ();
+        isLit = true;
+    }
+
+	private void Light()
+	{
+		GlobalLight.color = CandleColor;
+		//creepy noise;
+	}
+
+	private void Extinguish()
+	{
+		//play candle off sshhhh
+		Debug.Log("Candle extinguished");
+		GlobalLight.color = GlobalLightStartingColor;
+		Flame.intensity = 0f;
+	}
+}
