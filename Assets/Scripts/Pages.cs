0636e208e128b90400a6c2ade1c6100590d3f915
Thor <meghan.d.thor@gmail.com>
2016-01-30 21:52:05 -0500

    UI Work in progress

---
+++ Assets/Scripts/Pages.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pages : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetButtonDown ("ViewPages")) {
+			CanvasGroup.alpha = 1f; //show
+		} else 
+			CanvasGroup.alpha = 0f; //hide
+	}
+
+	void FlipPage () {
+
+	}
+
+}
